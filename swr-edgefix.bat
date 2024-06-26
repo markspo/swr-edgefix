de226d4da2751572aee07973d7232d909a58e6f0
markspo <65991415+markspo@users.noreply.github.com>
2024-06-26 12:29:10 -0400

    Update swr-edgefix.bat
    Added timestamp to backup folder

--- swr-edgefix.bat
+++ swr-edgefix.bat
@@ -1,4 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Get the current date and time
+for /f "tokens=2 delims==" %%i in ('"wmic os get localdatetime /value"') do set datetime=%%i
+
+:: Extract the date in YYYYMMDD format
+set timestamp=%datetime:~0,4%%datetime:~4,2%%datetime:~6,2%
+
+:: Terminate MS Edge
 taskkill /im msedge.exe /F
-mkdir "%LocalAppData%\Microsoft\Edge\User Data\Default-Backup\"
-xcopy "%LocalAppData%\Microsoft\Edge\User Data\Default\" "%LocalAppData%\Microsoft\Edge\User Data\Default-Backup\" /E/H
-move "%LocalAppData%\Microsoft\Edge\User Data\Default\Preferences" "%LocalAppData%\Microsoft\Edge\User Data\Default\Preferences.bak"
+
+:: Make backup directory
+mkdir "%LocalAppData%\Microsoft\Edge\User Data\Default-Backup-%timestamp%\"
+
+:: Copy Default user data to Backup directory
+xcopy "%LocalAppData%\Microsoft\Edge\User Data\Default\" "%LocalAppData%\Microsoft\Edge\User Data\Default-Backup-%timestamp%\" /E/H
+
+:: Rename the Preferences file
+move "%LocalAppData%\Microsoft\Edge\User Data\Default\Preferences" "%LocalAppData%\Microsoft\Edge\User Data\Default\Preferences.bak-%timestamp%"
+
+endlocal
